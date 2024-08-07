42ea44e2607b162031c1c55779ed9a0674149c86
skreuter <stefan.kreuter@gmail.com>
2024-08-07 06:28:10 +0200

    IMPROVE run.sh & ADD shell.sh

--- shell.sh
+++ shell.sh
@@ -3,4 +3,4 @@
 docker run --platform=linux/amd64 --rm -it --privileged \
   -v $(pwd)/output:/output \
   -v builder:/home/builder/live-build \
-  usb-cold-storage-builder /home/builder/build_live_usb.sh
+  usb-cold-storage-builder bash
