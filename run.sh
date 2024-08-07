a472c6efe44b3ad8e861e7dd159f6c99c90e6415
skreuter <stefan.kreuter@gmail.com>
2024-08-07 06:18:04 +0200

    IMPROVE run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker run --platform=linux/amd64 --rm -it --privileged -v $(pwd)/output:/output usb-cold-storage-builder /home/builder/build_live_usb.sh
+docker run --platform=linux/amd64 --rm -it --privileged \
+  -v $(pwd)/output:/output \
+  -v builder:/home/builder \
+  usb-cold-storage-builder /home/builder/build_live_usb.sh
